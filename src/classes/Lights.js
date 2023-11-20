602d4412c64b0069ff57269e4c6a84e1c1f25f0d
mattberg88 <mattbergart@gmail.com>
2023-11-19 22:05:56 -0800

    adjust lighting

--- src/classes/Lights.js
+++ src/classes/Lights.js
@@ -3,14 +3,14 @@ import { Group, SpotLight, PointLight, AmbientLight, HemisphereLight } from 'thr
 export default class BasicLights extends Group {
   constructor() {
     super();
-    const point = new PointLight(0xFFFFFF, 1, 10, 1);
-    const dir = new SpotLight(0xFFFFFF, 0.8, 7, 0.8, 1, 1);
-    const ambi = new AmbientLight( 0x404040 , 1);
-    const hemi = new HemisphereLight( 0xffffbb, 0x080820, 1.15 )
-    dir.position.set(5, 1, 2);
-    dir.target.position.set(0,0,0);
-    point.position.set(0, 1, 5);
-    point.intensity = 1;
-    this.add(point, ambi, hemi, dir);
+    const point = new PointLight(0xCCCCFF, 1, 10, 1);
+    const spot = new SpotLight(0xFFFFDD, 2, 3, 0.5, 1, 0.3);
+    const ambi = new AmbientLight( 0x404040 , 0.5);
+    const hemi = new HemisphereLight( 0xffffbb, 0x080820, 0.5)
+    spot.position.set(-1.5, 2, 0);
+    spot.target.position.set(0, 0.5, 0);
+
+    point.position.set(3, 3, 0);
+    this.add(point, ambi, hemi, spot, spot.target);
   }
 }
